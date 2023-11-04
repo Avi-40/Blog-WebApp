e143bde4f4ab6e6886e9c1a3102c51b6f4969650
Avi-40 <rag.s318@gmail.com>
2023-11-04 21:32:01 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+
+// express app
+const app = express();
+
+const dbURI = `mongodb+srv://netninja:test1234@cluster0.lgllxcm.mongodb.net/node-tuts?retryWrites=true&w=majority`;
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology:true})
+  .then((result)=>app.listen(3000,function(){
+    console.log("Server is running on port 3000")
+  }))
+  .catch((err)=>console.log(err));
+
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+
+// app.use(morgan('dev'));
+// app.use((req, res, next) => {
+//   res.locals.path = req.path;
+//   next();
+// });
+
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+//blogs
+app.use('/blogs',blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use((req, res, next) => {
+//   console.log('new request made:');
+//   console.log('host: ', req.hostname);
+//   console.log('path: ', req.path);
+//   console.log('method: ', req.method);
+//   next();
+// });
